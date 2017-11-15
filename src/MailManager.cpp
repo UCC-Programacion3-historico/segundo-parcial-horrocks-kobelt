501edac9c59a0212a6c0300eac046d7169346ddb
ianhorrocks <ianhorrocks11@gmail.com>
2017-11-15 07:02:40 -0300

    VOL 1.5

--- src/MailManager.cpp
+++ src/MailManager.cpp
@@ -74,10 +74,11 @@ vector<email> MailManager::getSortedByDate(string desde, string hasta) {
     vector<email> temporal, ret;
     temporal = arbolFecha.inorder();
     while(){
+
         if(temporal->getDate() >= desde && temporal->getDate() <= hasta){
             ret = temporal[];
         }
-    }
+
     return ret;
 }
 
